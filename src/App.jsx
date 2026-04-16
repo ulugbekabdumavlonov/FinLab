b49d1373ca1ebb7d522a9f789ef67b14212ec2fa
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-04-16 11:12:16 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./Сomponents/Layout";
+import Layout from "./Components/Layout";
 import LandingPage from "./Pages/LandingPage";
 
 
