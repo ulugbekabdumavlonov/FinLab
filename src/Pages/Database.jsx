4f7e31bfdc3ac2a489174ed9481823f4deaddd2c
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-04-17 15:46:51 +0500

    Update Database.jsx

--- src/Pages/Database.jsx
+++ src/Pages/Database.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import { auth, db } from "../firebase";
-import { store } from "../useAppStore";
+import { store } from "./useAppStore";
 import {
   collection,
   addDoc,
