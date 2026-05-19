5da02cbc6a54a448ba13519a16cdde1af7a666f7
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-05-19 11:36:40 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { AuthProvider } from "./Context/AuthContext";
+import { DataProvider } from "./Context/DataContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
