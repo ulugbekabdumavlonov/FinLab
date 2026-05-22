ef6d9822ceaf323367c017e1990e502232676480
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-05-22 16:22:20 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Layout from "./Components/Layout";
 import LandingPage from "./Pages/LandingPage";
-
-
 import Dashboard from "./Pages/Dashboard";
 import Cashflow from "./Pages/Cashflow";
 import PL from "./Pages/PL";
@@ -18,18 +15,34 @@ import MyCompany from "./Pages/MyCompany";
 import PrivateRoute from "./Components/PrivateRoute";
 import MyCategories from "./Pages/MyCategories";
 import MyProjects from "./Pages/MyProjects";
+import AccrualsPage from "./Pages/Accrualspage";
+import Employees from "./Pages/Employees";
+import SalaryStatement from "./Pages/SalaryStatement";
+import Counterpartiespage from "./Pages/Counterpartiespage";
+import Settlements from "./Pages/Settlements";
+import InvitePage from "./Pages/InvitePage";
+import Onboarding from "./Pages/Onboarding";
+import Warehouse from "./Pages/Warehouse";
+import Sales from "./Pages/Sales";
+import Purchases from "./Pages/Purchases";
+import Items from "./Pages/Items";
+import Helppage from "./Pages/Helppage";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         {/* 🔥 ЛЕНДИНГ */}
         <Route path="/" element={<LandingPage />} />
 
         {/* 🔐 AUTH */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+
+        {/* ✅ ИНВАЙТ — публичный, без авторизации */}
+        <Route path="/invite/:token" element={<InvitePage />} />
 
         {/* 🔒 ПРИЛОЖЕНИЕ (ЗАЩИЩЕННОЕ) */}
         <Route
@@ -51,8 +64,18 @@ function App() {
           <Route path="MyCompany" element={<MyCompany />} />
           <Route path="MyCategories" element={<MyCategories />} />
           <Route path="MyProjects" element={<MyProjects />} />
+          <Route path="AccrualsPage" element={<AccrualsPage />} />
+          <Route path="Employees" element={<Employees />} />
+          <Route path="Salarystatement" element={<SalaryStatement />} />
+          <Route path="Counterpartiespage" element={<Counterpartiespage />} />
+          <Route path="Settlements" element={<Settlements />} />
+          <Route path="Warehouse" element={<Warehouse />} />
+          <Route path="Sales" element={<Sales />} />
+          <Route path="Purchases" element={<Purchases />} />
+          <Route path="Items" element={<Items />} />
+          <Route path="Helppage" element={<Helppage />} />
+          <Route path="help" element={<Helppage />} />
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
