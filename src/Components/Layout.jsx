de0a74bca51af87256b6a80d339a646979a5e508
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-05-22 16:48:57 +0500

    Update Layout.jsx

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,7 +3,18 @@ import { Outlet, useLocation } from "react-router-dom";
 
 export default function Layout() {
   const location = useLocation();
-  const isFullscreen = location.pathname.toLowerCase().includes("operations");
+  const isFullscreen = location.pathname.toLowerCase().includes("operations") 
+                  || location.pathname.toLowerCase().includes("warehouse")
+                  || location.pathname.toLowerCase().includes("sales")
+                  || location.pathname.toLowerCase().includes("purchases")
+                  || location.pathname.toLowerCase().includes("items")
+                  || location.pathname.toLowerCase().includes("mywallet")
+                  || location.pathname.toLowerCase().includes("mycompany")
+                  || location.pathname.toLowerCase().includes("myproject")
+                  || location.pathname.toLowerCase().includes("mycategories")
+                  || location.pathname.toLowerCase().includes("counterpartiespage")
+                  || location.pathname.toLowerCase().includes("dashboard");
+  
 
   return (
     <div className="h-screen overflow-hidden flex flex-col bg-[#f8fafc]">
