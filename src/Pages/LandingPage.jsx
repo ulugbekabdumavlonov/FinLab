813bf6e2e4273494ac406f4fb1755641cd1da679
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-05-23 12:19:07 +0500

    Update LandingPage.jsx

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,7 +2,6 @@ import Header from "../Components/Header";
 import Hero from "../Components/Hero";
 import Features from "../Components/Features";
 import Logos from "../Components/Logos";
-import CTA from "../Components/CTA";
 import RegisterBlock from "../Components/RegisterBlock";
 import Footer from "../Components/Footer";
 
@@ -13,7 +12,6 @@ export default function LandingPage() {
       <Hero />
       <Features />
       <Logos />
-      <CTA />
       <RegisterBlock />
       <Footer />
     </div>
