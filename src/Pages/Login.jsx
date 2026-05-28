7f0cf258f8f26198bda58791dd9b8c79bf6ff4a0
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-05-28 23:30:49 +0500

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,7 +4,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
-import { auth } from "../../Services/firebase";
+import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Eye, EyeOff, ArrowRight } from "lucide-react";
