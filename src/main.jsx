9578fb91b43a2762264d39d10c9c2b9d51774264
ulugbekabdumavlonov <ulugbek.abdumavlonov@gmail.com>
2026-06-26 17:28:20 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,22 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./Context/AuthContext";
 import { DataProvider } from "./Context/DataContext";
+import { WarehouseProvider } from "./Pages/Warehouse/context/warehouseContext.jsx";
+import { ThemeProvider } from "./Context/ThemeContext";
+import { LanguageProvider } from "./Context/LanguageContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <DataProvider>
-        <App />
-      </DataProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <LanguageProvider>
+        <AuthProvider>
+          <DataProvider>
+            <WarehouseProvider>
+              <App />
+            </WarehouseProvider>
+          </DataProvider>
+        </AuthProvider>
+      </LanguageProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
